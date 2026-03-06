065d07b2db4043d22294a3415b8a340388cbb0b3
gayathripoluri <gayathrisameeraa@gmail.com>
2026-03-05 21:45:45 -0800

    Implemented AI code refactoring tool with Groq API and Next.js frontend

--- backend/src/services/refactorService.ts
+++ backend/src/services/refactorService.ts
@@ -2,11 +2,12 @@ import { openai } from "../config/openai"
 
 export const refactorCode = async (code: string) => {
   const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
+    model: "llama-3.3-70b-versatile",
     messages: [
       {
         role: "system",
-        content: "You are a senior software engineer who refactors code using SOLID and clean code principles. Return refactored code and explanation."
+        content:
+          "You are an expert software engineer.Refactor the given code to improve readability and maintainability.Avoid unnecessary functions or over-engineering.Keep the logic identical.Return only the improved code."
       },
       {
         role: "user",
